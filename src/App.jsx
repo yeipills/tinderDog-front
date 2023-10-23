d1d080abb7fc6b39f8e85a210deb6b0eb44b8859
yeipi <juanpablorosasmartin@gmail.com>
2023-10-23 01:52:31 -0300

    funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { CircularProgress } from '@mui/material';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(true);
+  const [dogImage, setDogImage] = useState('');
+  const [dogName, setDogName] = useState('');
+  const [acceptedDogs, setAcceptedDogs] = useState([]);
+  const [rejectedDogs, setRejectedDogs] = useState([]);
+
+  // Función para obtener una imagen aleatoria de perro
+  const fetchDogImage = async () => {
+    setIsLoading(true);
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await response.json();
+    setDogImage(data.message);
+    setDogName(generateRandomName());
+    setIsLoading(false);
+  };
+
+  // Función para generar un nombre aleatorio de 6 caracteres
+  const generateRandomName = () => {
+    return Math.random().toString(36).substr(2, 6);
+  };
+
+  // Lógica para los botones
+  const handleAccept = () => {
+    setAcceptedDogs([{ name: dogName, image: dogImage }, ...acceptedDogs]);
+    fetchDogImage();
+  };
+
+  const handleReject = () => {
+    setRejectedDogs([{ name: dogName, image: dogImage }, ...rejectedDogs]);
+    fetchDogImage();
+  };
+
+  useEffect(() => {
+    fetchDogImage();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      {/* Columna del Candidato */}
+      <div className="candidate">
+        {isLoading ? (
+          <CircularProgress />
+        ) : (
+          <>
+            <img src={dogImage} alt="Perro candidato" />
+            <p>{dogName}</p>
+            <button onClick={handleAccept}>Aceptar</button>
+            <button onClick={handleReject}>Rechazar</button>
+          </>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      {/* Columna de Aceptados */}
+      <div className="accepted">
+        {acceptedDogs.map((dog) => (
+          <img key={dog.name} src={dog.image} alt={dog.name} />
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {/* Columna de Rechazados */}
+      <div className="rejected">
+        {rejectedDogs.map((dog) => (
+          <img key={dog.name} src={dog.image} alt={dog.name} />
+        ))}
+      </div>
+
+      {/* Botón para arrepentirse (lo implementaremos en el siguiente paso) */}
+      <button>Arrepentirse</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
